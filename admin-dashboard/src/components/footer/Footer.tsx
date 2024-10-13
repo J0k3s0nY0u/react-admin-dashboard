fd08379ea2811e6e5f50d8a389d479cabb754bc7
J0k3s0nY0u <my.dev.vic@gmail.com>
2024-10-13 20:46:51 +0100

    did the footer properly

--- admin-dashboard/src/components/footer/Footer.tsx
+++ admin-dashboard/src/components/footer/Footer.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import "./footer.scss";
 
 const Footer = () => {
-    return (
-        <div className="footer">Footer</div>
-    )
-}
+  return (
+    <div className="footer">
+      <span>React Admin</span>
+      <span>
+        &copy; 2024 React Admin Dashboard.{" "}
+        <a href="https://github.com/J0k3s0nY0u">J0k3s0nY0u</a>
+      </span>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
