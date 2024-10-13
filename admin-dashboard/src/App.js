b63bd3c53d9726564107c5880724933e6dfea752
J0k3s0nY0u <my.dev.vic@gmail.com>
2024-10-13 17:34:24 +0100

    stuffff

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -1,43 +1,51 @@
 import Home from "./pages/home/Home.tsx";
 import Users from "./pages/users/Users.tsx";
 import Products from "./pages/products/Products.tsx";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar.tsx";
+import Menu from "./components/menu/Menu.tsx";
+import Footer from "./components/footer/Footer.tsx";
 
 function App() {
-
   const Layout = () => {
     return (
       <div className="main">
         <Navbar />
-        <Menu />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
       </div>
-    )
-  }
+    );
+  };
 
-  const router = createBrowserRouter ([
+  const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
-    },
-    {
-      path: "users",
-      element: <Users />,
-    },
-    {
-      path: "products",
-      element: <Products />,
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        },
+      ],
     },
   ]);
 
-  return (
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
