934862cebfc8f517636797d9a235a4d4c836d60a
J0k3s0nY0u <my.dev.vic@gmail.com>
2024-10-13 23:46:40 +0100

    tired, continue tomorrow

---
+++ admin-dashboard/src/data.ts
@@ -0,0 +1,116 @@
+export const menu = [
+  {
+    id: 1,
+    title: "main",
+    listItems: [
+      {
+        id: 1,
+        title: "Homepage",
+        url: "/",
+        icon: "fa-solid fa-house",
+      },
+      {
+        id: 2,
+        title: "Profile",
+        url: "/users/1",
+        icon: "fa-solid fa-user",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "lists",
+    listItems: [
+      {
+        id: 1,
+        title: "Users",
+        url: "/users",
+        icon: "fa-solid fa-users",
+      },
+      {
+        id: 2,
+        title: "Products",
+        url: "/products",
+        icon: "fa-solid fa-people-carry-box",
+      },
+      {
+        id: 3,
+        title: "Orders",
+        url: "/orders",
+        icon: "fa-solid fa-arrow-up-wide-short",
+      },
+      {
+        id: 4,
+        title: "Posts",
+        url: "/posts",
+        icon: "fa-solid fa-clipboard",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "general",
+    listItems: [
+      {
+        id: 1,
+        title: "Elements",
+        url: "/",
+        icon: "fa-solid fa-people-carry-box",
+      },
+      {
+        id: 2,
+        title: "Notes",
+        url: "/",
+        icon: "fa-solid fa-people-carry-box",
+      },
+      {
+        id: 3,
+        title: "Forms",
+        url: "/",
+        icon: "fa-solid fa-people-carry-box",
+      },
+      {
+        id: 4,
+        title: "Calender",
+        url: "/",
+        icon: "fa-solid fa-people-carry-box",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "maintenance",
+    listItems: [
+      {
+        id: 1,
+        title: "Settings",
+        url: "/",
+        icon: "fa-solid fa-people-carry-box",
+      },
+      {
+        id: 2,
+        title: "Backups",
+        url: "/",
+        icon: "fa-solid fa-people-carry-box",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "analytics",
+    listItems: [
+      {
+        id: 1,
+        title: "Charts",
+        url: "/",
+        icon: "fa-solid fa-people-carry-box",
+      },
+      {
+        id: 2,
+        title: "Logs",
+        url: "/",
+        icon: "fa-solid fa-people-carry-box",
+      },
+    ],
+  },
+];
