713c916d530f1ce13f5088ab94d248f9f008d75c
J0k3s0nY0u <my.dev.vic@gmail.com>
2024-10-26 17:56:42 +0100

    stuff

--- admin-dashboard/src/components/chartBox/ChartBox.tsx
+++ admin-dashboard/src/components/chartBox/ChartBox.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./chartBox.scss";
 import { Link } from "react-router-dom";
-import { ResponsiveContainer, LineChart, Line } from "recharts";
+import { ResponsiveContainer, LineChart, Line, Tooltip } from "recharts";
 
 const data = [
   {
@@ -63,11 +63,17 @@ const ChartBox = () => {
         <div className="chart">
           <ResponsiveContainer width="100%" height="100%">
             <LineChart width={300} height={100} data={data}>
+              <Tooltip
+                contentStyle={{ background: "transparent", border: "none" }}
+                labelStyle={{ display: "none" }}
+                position={{ x: 10, y: 60 }}
+              />
               <Line
                 type="monotone"
                 dataKey="pv"
                 stroke="#8884d8"
                 strokeWidth={2}
+                dot={false}
               />
             </LineChart>
           </ResponsiveContainer>
